a75f1028b119fa6f48a2510d34f55fe167e35f07
Daxi90 <hdateam90@gmail.com>
2023-10-22 11:51:28 +0200

    started implements router

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Importiere Router
 
 @Component({
   selector: 'app-start-screen',
   templateUrl: './start-screen.component.html',
   styleUrls: ['./start-screen.component.scss']
 })
-export class StartScreenComponent {
+export class StartScreenComponent implements OnInit {
 
+  constructor(private router: Router) {}
+
+  newGame() {
+    this.router.navigate(['/game']); // Navigiere zur Game Route
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
