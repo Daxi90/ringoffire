e7606cc87db68a8d2b64b3e2bf5055784a380687
Daxi90 <hdateam90@gmail.com>
2023-10-23 19:06:38 +0200

    started implement shuffle cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
   pickCardAnimation = false;
+  game: Game;
+
+  constructor(){
+    this.game = new Game();
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
 
   takeCard(){
     this.pickCardAnimation = true;
+  }
+
+  newGame(){
+    console.log(this.game);
   }
 
 }
